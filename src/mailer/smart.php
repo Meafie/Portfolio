ef7eafa55ac11595972c9f01ccbc00a9e73aaaef
Ivan Romanov <rmnv.ivn@gmail.com>
2023-11-01 21:15:58 +0400

    Release

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -11,14 +11,14 @@ $mail->CharSet = 'UTF-8';
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->Host = 'mail.romanov-web.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'blindeagleru@gmail.com';                 // Наш логин
-$mail->Password = 'pqqm ztfz eokr isut';                           // Наш пароль от ящика
+$mail->Username = 'info@romanov-web.ru';                 // Наш логин
+$mail->Password = 'zJ3lB3xR4a';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('blindeagleru@gmail.com', 'Романов Иван');   // От кого письмо 
+$mail->setFrom('info@romanov-web.ru', 'Романов Иван');   // От кого письмо 
 $mail->addAddress('rmnv.ivn@gmail.com');     // Add a recipient
 $mail->isHTML(true);                                  // Set email format to HTML
 
